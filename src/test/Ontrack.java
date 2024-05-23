2cbb41b68223a5b485a99ae0a7119ff73f6cfe5e
patelgruhi <150305781+patelgruhi@users.noreply.github.com>
2024-05-23 01:10:32 -1000

    Update Ontrack.java

--- src/test/Ontrack.java
+++ src/test/Ontrack.java
@@ -1,12 +1,24 @@
-package test;
+package task_10P;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Ontrack {
+public class OnTrack {
 
-	public Map<String, Object> viewTaskDetails(int i, int j) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+    public Map<String, Object> viewTaskDetails(int taskId, int studentId) {
+      
+        Map<String, Object> taskDetails = new HashMap<>();
+        taskDetails.put("tid", 1);
+        taskDetails.put("descrip", "707 task");
+        taskDetails.put("submission", "2024-05-20");
+        taskDetails.put("feedback", "nice work.");
+        List<Map<String, String>> chatHistory = Arrays.asList(
+            new HashMap<String, String>() {{ put("sender", "student"); put("message", "i am having a problem"); }},
+            new HashMap<String, String>() {{ put("sender", "tutor"); put("message", "Sure, what do you need help with?"); }}
+        );
+        taskDetails.put("chat_history", chatHistory);
+        return taskDetails;
+    }
 }
